585d41e784994f2a3cd115aaa93a246d769f3b0a
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-23 18:40:55 +0100

    Add routes  customers and products

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+
+const routes = [
+    {path: '/customers', component: AppCustomers},
+    {path: '/products', component: AppProducts}
+];
+
+const route = new VueRouter ({
+  routes
+});
 
 new Vue({
   render: h => h(App),
